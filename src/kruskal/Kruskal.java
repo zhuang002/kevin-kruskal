76251e729d925e8c87a49f573f4ee2293c406303
Belrog76 <xuanyi159753@gmail.com>
2020-08-25 15:24:31 -0400

    1

--- src/kruskal/Kruskal.java
+++ src/kruskal/Kruskal.java
@@ -6,6 +6,7 @@
 package kruskal;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -14,14 +15,16 @@ import java.util.Scanner;
  */
 public class Kruskal {
 
-    static private ArrayList<Path> graph=new ArrayList();
-    static private ArrayList<Integer[]> subtrees= new ArrayList();
-    static private ArrayList<Path> spanTree=new ArrayList();
+    static private ArrayList<Path> graph = new ArrayList();
+    static private int[] subTrees = null;
+    static private ArrayList<Path> spanTree = new ArrayList();
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
         readInput();
         processSpanTree();
         printPathLength();
@@ -31,7 +34,21 @@ public class Kruskal {
      * read input into graph.
      */
     private static void readInput() {
-        
+        Scanner sc = new Scanner(System.in);
+        int nodes = sc.nextInt();
+        int path = sc.nextInt();
+        for (int i = 0; i < path; i++) {
+            Path p = new Path();
+            p.node1 = sc.nextInt();
+            p.node2 = sc.nextInt();
+            p.length = sc.nextInt();
+            graph.add(p);
+        }
+        Collections.sort(graph);
+        subTrees = new int[nodes];
+        for (int i = 0; i < subTrees.length; i++) {
+            subTrees[i] = -1;
+        }
     }
 
     /**
@@ -39,9 +56,11 @@ public class Kruskal {
      */
     private static void processSpanTree() {
         while (true) {
-            ArrayList<Path> shortestPaths=getShortPaths();
-            if (shortestPaths.isEmpty()) break;
-            for (Path path:shortestPaths) {
+            ArrayList<Path> shortestPaths = getShortPaths();
+            if (shortestPaths.isEmpty()) {
+                break;
+            }
+            for (Path path : shortestPaths) {
                 if (shouldAddToSpanTree(path)) {
                     addToSpanTree(path);
                 }
@@ -53,72 +72,113 @@ public class Kruskal {
      * Print the generated spanning tree from spanTree.
      */
     private static void printPathLength() {
+        int sum = 0;
+        for (int i = 0; i < spanTree.size(); i++) {
+            sum += spanTree.get(i).length;
+        }
+        System.out.println(sum);
     }
 
     /**
      * Get the left shortest paths from graph.
+     *
      * @return return a list of paths that has the shortest distance.
      */
     private static ArrayList<Path> getShortPaths() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<Path> sPath = new ArrayList();
+        for (int i = 0; i < graph.size(); i++) {
+            if (graph.get(0).length == graph.get(i).length) {
+                sPath.add(graph.get(i));
+            } else {
+                break;
+            }
+        }
+        graph.removeAll(sPath);
+        return sPath;
     }
-
-    
 
     /**
      * Add a path to the spanTree and subTrees
-     * @param path 
+     *
+     * @param path
      */
     private static void addToSpanTree(Path path) {
-        
+        spanTree.add(path);
+        if (subTrees[path.node1] == -1) {
+            if (subTrees[path.node2] == -1) {
+                subTrees[path.node1] = path.node1;
+                subTrees[path.node2] = path.node1;
+            } else {
+                subTrees[path.node1] = subTrees[path.node2];
+            }
+        } else {
+            if (subTrees[path.node2] == -1) {
+                subTrees[path.node2] = subTrees[path.node1];
+            } else {
+                int r1 = subTrees[path.node1];
+                int r2 = subTrees[path.node2];
+                for (int i = 0; i < subTrees.length; i++) {
+                    if (subTrees[i] == r2) {
+                        subTrees[i] = r1;
+                    }
+                }
+            }
+        }
     }
 
     /**
      * Check if any of the subtrees contains the node.
+     *
      * @param node The node to check
-     * @return true for there is one subtree contains the node, false for there is no subtree contains the node.
+     * @return true for there is one subtree contains the node, false for there
+     * is no subtree contains the node.
      */
     private static boolean subtreesContains(int node) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return subTrees[node] != -1;
     }
 
     /**
      * Get the root id of the subtree that contains the node.
+     *
      * @param node the node to find its subtree.
-     * @return the root id of the subtree if node is in a subtree, otherwise return -1.
+     * @return the root id of the subtree if node is in a subtree, otherwise
+     * return -1.
      */
     private static int getSubtree(int node) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return subTrees[node];
     }
 
     /**
-     * Check if a new path should be added to the span tree. 
+     * Check if a new path should be added to the span tree.
+     *
      * @param path the path to be checked.
      * @return true if should be added, otherwise false.
      */
     private static boolean shouldAddToSpanTree(Path path) {
-        if (!subtreesContains(path.node1)|| !subtreesContains(path.node2)) {
+        if (!subtreesContains(path.node1) || !subtreesContains(path.node2)) {
             return true;
-        } 
-        if (getSubtree(path.node1)!= getSubtree(path.node2)) {
+        }
+        if (getSubtree(path.node1) != getSubtree(path.node2)) {
             return true;
         }
         return false;
     }
-    
+
 }
 
 /**
  * The class that contains a path info.
+ *
  * @author zhuan
  */
 class Path implements Comparable {
+
     public int node1; // one end of a path.
     public int node2; // the other end of a path.
     public int length; // the length of the path.
 
     @Override
     public int compareTo(Object o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.length - ((Path) o).length;
     }
 }
